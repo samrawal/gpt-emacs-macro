e1ba350104774f42e74c5c64886d64753eae9e6c
Sam Rawal <scrawal2@illinois.edu>
2022-11-13 00:20:46 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gpt-macro
 Connect GPT-3 to Emacs and run "semantic" kbd macros
+
+# About
+[![About](https://raw.githubusercontent.com/samrawal/gpt-macro/main/assets/tweet.png)](https://twitter.com/samarthrawal/status/1591527892386734081)
+
+![Demo](./assets/emacs-gpt-macro-2.gif)
+
+# Usage
+This was a super quick implementation and currently uses the OpenAI Python package and interfaces with a Python script. This can be simplified to elisp-only via curl.
+
+Add your OpenAI API key and set the Python script path in `gpt-macro.el` 
---
+++ gpt-macro.el
@@ -0,0 +1,23 @@
+(setq openai-api-key "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+(defun gpt-macro-call (api-key prompt data)
+  (setq cmd (concat "python3.9 /path-to-project/gpt-macro/gpt-macro.py \"" api-key "\" \"" prompt "\" \"" data "\""))
+  (shell-command-to-string cmd)
+)
+
+(defun gpt-macro()
+  (interactive)
+  (setq selection  (buffer-substring (region-beginning) (region-end)))
+  (setq gpt-macro-input (read-string "Command: "))
+  (delete-region (region-beginning) (region-end))
+  (insert (gpt-macro-call openai-api-key gpt-macro-input selection))
+)
+
+(defun gpt-macro-noreplace()
+  (interactive)
+  (setq selection  (buffer-substring (region-beginning) (region-end)))
+  (setq gpt-macro-input (read-string "Command: "))
+  (insert (gpt-macro-call openai-api-key gpt-macro-input selection))
+)
+
+(global-set-key (kbd "C-c g") 'gpt-macro)
