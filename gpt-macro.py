555c824d60ec947aa8e3bcd3c53d6efd5ce1f22e
Sam Rawal <scrawal2@illinois.edu>
2023-03-07 22:13:07 -0600

    added ChatGPT option in Python script (requires openai==0.27.0 Python package)

--- gpt-macro.py
+++ gpt-macro.py
@@ -6,15 +6,31 @@ openai.api_key = sys.argv[1]
 gpt_macro_input = sys.argv[2]
 data = sys.argv[3]
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="{}:\n\n{}".format(gpt_macro_input, data),
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+def complete(input_, data_, model="davinci"):
+    prompt = "{}:\n\n{}".format(input_, data_)
+    if model == "davinci":
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        return response['choices'][0]['text']
+    elif model == "chat":
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            temperature=1.0, # default
+            max_tokens=256,
+            top_p=1,
+        )
+        return completion.choices[0]["message"]["content"]
 
-payload = response['choices'][0]['text'].strip()
+
+payload = complete(gpt_macro_input, data, model="davinci").strip()
 print(payload)
